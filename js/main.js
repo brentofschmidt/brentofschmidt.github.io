0b7ee93281f5c3b4c89df9f64f2c3bbf69233ea5
bschmidt <brentofschmidt@gmail.com>
2020-02-09 16:12:32 -0600

    Checkpoint.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const mainImg = document.querySelector("#main-img");
+
+const scale = 0.05;
+
+document.addEventListener("mousemove", (e) => {
+  mainImg.style.backgroundPositionX = -e.screenX * scale + "px";
+  mainImg.style.backgroundPositionY = -e.screenY * scale + "px";
+
+  let mox = ((e.screenX - (screen.width * 0.5)) / screen.width);
+  let moy = ((e.screenY - (screen.height * 0.5)) / screen.height);
+});
